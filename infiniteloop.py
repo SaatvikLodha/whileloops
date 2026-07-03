266f54e90520d3a794f808e2ffa863499acee403
SaatvikLodha <saatviklodha@gmail.com>
2026-07-03 15:24:13 -0500

    Add files via upload

---
+++ infiniteloop.py
@@ -0,0 +1,15 @@
+print("Welcome to the QUIZ!🥳")
+print("What is the capital of the USA?")
+print("A: Houston")
+print("B: Washington DC")
+print("C: Chicago")
+print("D: None of the above")
+answer=str(input("Please enter A, B, C, or D as your answer choice:"))
+if answer=="B":
+    print("Congratulations! You passed the QUIZ!!!🥳")
+else:
+    i="A" or "C" or "D"
+    while answer==i:
+        print("😭")
+                 
+
